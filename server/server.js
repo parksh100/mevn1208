d9650691c9c8f81207dde700ce20f5f4d5194d0b
shpark <parksh1000@outlook.com>
2021-12-09 16:45:54 +0900

    라우터 전단계

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ let corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const db = require('./app/models');
 db.mongoose.connect(db.url, {
   useNewUrlParser: true,
@@ -24,12 +27,13 @@ db.mongoose.connect(db.url, {
   });
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to self application.' })
 });
+
+// require('./app/routes/tutorial.routes')(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
